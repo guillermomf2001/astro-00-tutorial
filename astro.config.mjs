d4aad99d291d28369ed98ca0f13e7637a5457a47
guillermomf2001 <guillermomf2001@gmail.com>
2025-03-08 18:40:56 +0100

    6.1 Construye tu primera isla de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://astro-00-tutorial.netlify.app/"
+  site: "https://astro-00-tutorial.netlify.app/",
+  integrations: [preact()]
 });
